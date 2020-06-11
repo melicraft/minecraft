c26034ab2ce32d097f1845e58a2328a1015f2f5c
roqueribeiro <roque.ribeiro@hotmail.com.br>
2020-06-11 14:49:32 -0300

    Auto Backup Server ${$(date +%d-%m-%Y)}

--- backup.sh
+++ backup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 printf "Start Backup Job"
+sudo chown -R roque: /srv/minecraft
 git add .
 git commit -m 'Auto Backup Server ${$(date +'%d-%m-%Y')}' 
 git push 
+sudo chown -R minecraft.minecraft /srv/minecraft
+sudo setfacl -R -m u:minecraft:rwx /srv/minecraft
 printf "Backup Finished"
